c5ba84a7254572a56c99278789b611a8c5a0e910
joeyf <g00361752@gmit.ie>
2021-05-03 20:21:50 +0100

    Creating location marker component

---
+++ components/LocationMarker.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Icon } from '@iconify/react'
+import locationIcon from '@iconify-icons/mdi/fish'
+
+export default function LocationMarker(props){
+    return(
+        <div className="location-marker" >
+            <Icon icon={locationIcon} className="location-icon" width="1.5em" height="1.5em"/>
+        </div>
+    )
+    
+}
