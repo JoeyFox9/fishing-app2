64153e882d40d3c9163242653ffcdf9ad3369659
joeyf <g00361752@gmit.ie>
2021-05-08 19:28:04 +0100

    adding addcatch url to constants

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 //API URL
-export const API_URL = 'http://10.0.0.9:3000';
+export const API_URL = 'http://10.0.0.6:3000';
 
 //API End Points
 export const REGISTER = `${API_URL}/user/signup`;
@@ -11,3 +11,4 @@ export const UPDATE_PROFILE = `${API_URL}/user`;
 export const UPLOAD_IMAGE = `${API_URL}/user/upload`;
 export const FORGOT_PASSWORD = `${API_URL}/auth/recover`;
 export const CATCHES = `${API_URL}/user/catches`
+export const ADDCATCH = `${API_URL}/user/addCatch`;
